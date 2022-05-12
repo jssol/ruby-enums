78ee36d003ccaf5dd7ec43629218a62717065b6e
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-12 16:11:44 +0200

    Create the list

---
+++ my_list.rb
@@ -0,0 +1,30 @@
+require_relative './my_enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
+end
+
+# test the provided test-cases
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
+
+puts(list.filter(&:even?))
+list.each { |e| puts e * e }
+
