cbb1ef6691f00a444c1de39da6b8afd19c1f38cf
bushmusi <bushera.mestofa@gmail.com>
2022-05-13 08:47:34 +0300

    Add enumerable file

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,37 +1,34 @@
-# module MyEnumerable
-#   def list
-#     @list ||= []
-#   end
-#
-#   # check if all conditions are met
-#   def all?
-#     meets_condition = []
-#
-#     list.each do |element|
-#       meets_condition.push(element) if yield element
-#     end
-#     meets_condition.length == list.length
-#   end
-#
-#   # check if any single condition is met
-#   def any?
-#     one_condtion_met = false
-#     list.each do |element|
-#       if yield element
-#         one_condtion_met = true
-#         break
-#       end
-#     end
-#     one_condtion_met
-#   end
-#
-#   # filter items pe the provided condtion
-#   def filter
-#     meets_condition = []
-#
-#     list.each do |element|
-#       meets_condition.push(element) if yield element
-#     end
-#     meets_condition
-#   end
-# end
+module MyEnumerable
+  def list
+    @list ||= []
+  end
+
+  def all?
+    meets_condition = []
+
+    list.each do |element|
+      meets_condition.push(element) if yield element
+    end
+    meets_condition.length == list.length
+  end
+
+  def any?
+    one_condtion_met = false
+    list.each do |element|
+      if yield element
+        one_condtion_met = true
+        break
+      end
+    end
+    one_condtion_met
+  end
+
+  def filter
+    meets_condition = []
+
+    list.each do |element|
+      meets_condition.push(element) if yield element
+    end
+    meets_condition
+  end
+end
